c2eb1df41ef9057722c8e71d8b2012b1397046be
Risent <bsstr@yandex.ru>
2015-02-18 20:51:05 +0300

    simple creation for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,31 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(article_params)
+    @card.save
+    redirect_to cards_path
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def show
+    
+  end
+  private
+    def article_params
+      params.require(:card).permit(
+      :original_text, :translated_text)
+    end
 end

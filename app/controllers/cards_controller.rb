5cdde08f3b2d023ed0b47d74d71389f92561edc0
Risent <bsstr@yandex.ru>
2015-02-19 01:01:25 +0300

    code refactoring

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,12 @@
 class CardsController < ApplicationController
+  before_action :find_card, only: [:edit, :show, :update, :destroy]
+
   def index
     @cards = Card.all
   end
 
   def new
     @card = Card.new
-    @card.review_date = Time.now + 3.days
   end
 
   def create
@@ -18,11 +19,9 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def update
-    @card = Card.find(params[:id])
     if @card.update_attributes(card_params)
       redirect_to @card
     else
@@ -31,18 +30,19 @@ class CardsController < ApplicationController
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.destroy if @card
     redirect_to cards_path
   end
 
   private
+    def find_card
+      @card = Card.find(params[:id])
+    end
+
     def card_params
-      params.require(:card).permit(
-      :original_text, :translated_text)
+      params.require(:card).permit(:original_text, :translated_text)
     end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,18 @@
 class Card < ActiveRecord::Base
-  validates :original_text,   presence: true
-  validates :translated_text, presence: true
-  validates :review_date, presence: true
-  validate :different_words
+  after_initialize :set_date
 
-  def different_words
-    errors.add(:original_text, "Тексты совпадают") if 
-    UnicodeUtils.downcase(self.original_text) == UnicodeUtils.downcase(self.translated_text)  
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :texts_is_different
+
+  def texts_is_different
+    if UnicodeUtils.downcase(self.original_text) == UnicodeUtils.downcase(self.translated_text)
+      errors.add(:original_text, "Тексты совпадают") 
+    end  
   end
+
+  private
+    def set_date
+      self.review_date = Time.now + 3.days
+    end
+
 end

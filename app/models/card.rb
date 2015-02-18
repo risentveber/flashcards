4b9890e6f094482109688649cdeb0a8187e5b49d
Risent <bsstr@yandex.ru>
2015-02-18 23:23:39 +0300

    add validation for different words

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,11 @@
 class Card < ActiveRecord::Base
-	validates :original_text,   presence: true
-	validates :translated_text, presence: true
-	validates :review_date, presence: true
+  validates :original_text,   presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :different_words
+
+  def different_words
+    errors.add(:original_text, "Тексты совпадают") if 
+    UnicodeUtils.downcase(self.original_text) == UnicodeUtils.downcase(self.translated_text)  
+  end
 end

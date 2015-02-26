4a48cfceb12e52ca74b945ee634ff3547b9bed20
Risent <bsstr@yandex.ru>
2015-02-26 23:18:13 +0300

    add simple fill in

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,28 @@ require 'rails_helper'
 feature "Homepage" do
   context "homepage has some buttons" do
 
+    before :all do
+      @card = Card.create original_text: 'Home', translated_text: 'Дом', review_date: Time.now
+    end
+
+    after :all do
+      @card.destroy!
+    end
+
+    it "show that answer is wrong" do
+      visit root_path
+      fill_in :translation, with: "что-то там"
+      click_button "Проверить"
+      expect(page).to have_content "Ошибка"
+    end
+
+     it "show that answer is right" do
+      visit root_path
+      fill_in :translation, with: "дом"
+      click_button "Проверить"
+      expect(page).to have_content "Успех"
+    end
+
     it "has all cards button" do
       visit root_path
       expect(page).to have_content "Все карточки"
